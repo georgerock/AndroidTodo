31fd41bfc093e4ea7de7384a9399a6e133f98ceb
Cosma George <cgeorge@192.168.1.4>
2017-07-09 11:26:48 +0300

    Initial settings

---
+++ app/src/main/java/com/whitecitycode/cgeorge/notetoself/Note.java
@@ -0,0 +1,15 @@
+package com.whitecitycode.cgeorge.notetoself;
+
+/**
+ * Created by cgeorge on 7/9/17.
+ */
+
+public class Note {
+
+    private String mTitle;
+    private String mDescription;
+    private Boolean mIdeea;
+    private Boolean mTodo;
+    private Boolean mImportant;
+
+}
